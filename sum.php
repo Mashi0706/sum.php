98cec16d86b8e0f67e4e9643beb4c2b20a04d5fb
Mashi <mune.andmore@gmail.com>
2020-10-09 20:08:14 +0000

    Kadai teishutu

--- sum.php
+++ sum.php
@@ -2,27 +2,29 @@
 echo 'hello php!';
 
 // 1.引数に数値を指定して実行すると、数値を２倍にして返す関数を作成する。カンニング済
-function sum($max) {
-    $result= $max *2;
-    echo $result;
+function double($max) {
+    return $max *2;
 }
-
-echo sum(50);
-echo "\n";
+echo sum(50); 
 
 
 // 2.$aと$bを仮引数に持ち、$aと$bを足した結果を返す関数を作成。
-function f($a, $b) {
- $result = f($a + $b);
- }
+function f($a, $b): float{
+    return $a + $b;
+}
+    
 
 
- // 3. $arr という配列の仮引数を持ち、数値が入った配列array(1,3,5,7,9)
- //を渡すとその要素をすべてかけた結果を返す関数を作成する。カンニング済
- $arr = array("1","3","5","7","9");
-  foreach ($arr as $count) {
-      echo $count .'<br>';
+ //3. $// 3.$arr という配列の仮引数を持ち、数値が入った配列array(1, 3, 5 ,7, 9) を渡すとその要素をすべてかけた結果を返す関数を作成してください。
+ 
+ function product($arr) {
+     $result = $arr[0];
+     for ($i = 1; $i< count($arr); $i++) {
+           $result *= $arr[$i];
+     }
+     return $result;
  }
+ echo product(array(1,3,5,7,9));
   
   
  //4.【応用】次のプログラムは、配列の中で１番大きい値を返すmax_rryと言う関数を実装しようとしてます。　途中の部分を完成させる。
@@ -30,10 +32,13 @@ function max_array ($arr) {
 //とりあえず配列の最初の要素を一番大きい値とする。
 $max_number = $arr [0];
 foreach ($arr as $a) {
-  $max_number<[0];
+   if($max_number < $a){
+       $max_number = $a;
+   }
 }
 return $max_number; 
  } 
+ echo max_array(array(6,2,3,10,5));
 
 
 // 5. 次のビルトイン関数の用途、使い方を調べて実際に使用してみる。
@@ -56,7 +61,7 @@ $array1= array('a','b','c');
 $array2= array('d','e','f');
 print_r(array_merge($array1, $array2));
 
-//time,mktime
+//t
 //<?php
 $time = mktime(9);
 var_dump(date('Y年m月d日h時i分s秒',$time));
